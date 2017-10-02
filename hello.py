ba88c53c2b593baf32d7e0240639706f48620c06
fairy <fairy.freyja@gmail.com>
2017-10-02 18:48:20 +0300

    clean up folder static and rename file concerts.html

--- hello.py
+++ hello.py
@@ -10,13 +10,13 @@ def index():
 def contact():
 	return render_template('contact.html')
 	
-@app.route("/conserts")
-def conserts():
-	return render_template('conserts.html')
+@app.route("/concerts")
+def concerts():
+	return render_template('concerts.html')
 	
 @app.route("/listen")
 def listen():
 	return render_template('listen.html')
  
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000)
+    app.run(host='127.0.0.1', port=4080)
